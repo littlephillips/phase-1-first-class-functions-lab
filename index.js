3527c613e95246ea4cc4c6a3f9933a0794eea842
littlephillips <phillipkinuthia72@gmail.com>
2022-06-08 15:15:26 +0300

    edited index.js file

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+ const drivers = [`Antonia`, 'Nuru', `Amari`, `Mo`];
+
+ function returnFirstTwoDrivers (drivers){
+     return drivers.slice(0,2)
+ }
+
+function returnLastTwoDrivers (drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (multiplier) {
+    return (value)=>{
+       return multiplier * value
+    }
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (drives, returnDrivers) { 
+
+    return returnDrivers(drivers)
+
+
+}
+
+
+
+
+
+
